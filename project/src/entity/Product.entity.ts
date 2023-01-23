fbea13db1f2052ce45eaedca79c310243cdd8205
diasEduardo <eduardo_dududex@hotmail.com>
2023-01-23 11:42:49 -0300

    product crud

--- project/src/entity/Product.entity.ts
+++ project/src/entity/Product.entity.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty, isNotEmpty, Length } from "class-validator";
 import {Entity,Column,PrimaryColumn } from "typeorm"
 import {v4 as uuid} from 'uuid'
 
@@ -5,12 +6,20 @@ import {v4 as uuid} from 'uuid'
 export class Product{
     @PrimaryColumn({name:`ID_PRODUCT`,type:`varchar`,length:255})
     idProduct: string;
+
     @Column({name:`NM_PRODUCT`,type:`varchar`,length:255}) // column type is case sensitive and lowercased
+    @IsNotEmpty()
+    @Length(0,255)
     nmProduct: string;
+
     @Column({name:`DE_PRODUCT`,type:`varchar`,length:255,nullable:true})
+    @Length(0,255)
     deProduct: string;
+
     @Column({name:`VL_WEIGHT`,type:`int`})
+    @IsNotEmpty()
     vlWeight:number;
+
     @Column({name:`DT_REGISTER`,type:`datetime`,default:()=>`GETDATE()` })
     dtRegister:Date;
 
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,15 +1,16 @@
-import express from 'express'
-import './database/connection'
+import express from 'express';
+import '@/database/connection';
+import ProductController from '@/controller/Product.controller';
+import bodyParser, { BodyParser } from 'body-parser';
+import cors from 'cors';
 
 const PORT =  parseInt(process.env.PORT|| '8080');
 const INDEX_PATH: string =  process.env.INDEX_PATH || '';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 app.listen(PORT,():void=>{
     console.log(`Server running in :${PORT}`)
 });
-app.get(INDEX_PATH,():void=>{
-    console.log(`index running at "${INDEX_PATH}"`)
-})
-
 
